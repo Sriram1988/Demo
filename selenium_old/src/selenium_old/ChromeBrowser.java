ff1eb953d79f215a214f3ef88cdb8dc2e5d6cd51
jesus <jesus@host.docker.internal>
2023-12-15 18:25:06 +0530

    sriram

---
+++ selenium_old/src/selenium_old/ChromeBrowser.java
@@ -0,0 +1,14 @@
+package selenium_old;
+
+
+
+public class ChromeBrowser {
+
+	public static void main(String[] args) {
+		System.out.println("ChromeBrowser lanch");
+		System.setProperty("sriram","\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk\"");
+	
+
+	}
+
+}
